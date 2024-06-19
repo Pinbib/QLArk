f1680284d7fd7db0df56177d10201e0d9ed231b2
Pinbib <pancenkoe353@gmail.com>
2024-06-19 09:13:03 +0100

    0.0.7

--- build.js
+++ build.js
@@ -1,24 +1,8 @@
 const {exec} = require("child_process");
-const fs = require("fs");
-const path = require("path");
 const Console = require("./src/Console.js");
 
-const VERSION = "0.0.7";
-
 const platforms = ["linux", "macos", "win"];
-
-const file = path.parse(path.basename("./src/QLArk.js")).name;
-
-
 platforms.forEach(el => {
-	if (fs.existsSync(path.join("./", "PKG", el, file)) || fs.existsSync(path.join("./", "PKG", el, file + ".exe"))) {
-		if (el === "win") {
-			fs.renameSync(path.join("./", "PKG", el, file + ".exe"), path.join("./", "PKG", el, file + VERSION + ".exe"));
-		} else {
-			fs.renameSync(path.join("./", "PKG", el, file), path.join("./", "PKG", el, file + VERSION));
-		}
-	}
-
 	exec(`npx pkg ./src/QLArk.js --targets node18-${el} --output ./PKG/${el}/QLArk`, (err) => {
 		if (err) {
 			Console.error(`Error witch building file in the ${el}.`);
