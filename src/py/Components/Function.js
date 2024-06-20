6692c363c124493f363d7d06ade4f43f3739555e
Pinbib <pancenkoe353@gmail.com>
2024-06-20 11:34:52 +0100

    variable visibility has been fixed

--- src/py/Components/Function.js
+++ src/py/Components/Function.js
@@ -1,11 +1,11 @@
 // def {name}():
-//	global {global}
+//	nonlocal {global}
 // 	return {value()}
 //
 
 class Func extends String {
 	constructor(name, value, global) {
-		super(`\t\t\tdef ${name}():\n\t\t\t\tglobal ${global}\n\t\t\t\treturn ${value()}\n\t\t\t\n`);
+		super(`\t\t\tdef ${name}():\n\t\t\t\tnonlocal ${global}\n\t\t\t\treturn ${value()}\n\t\t\t\n`);
 	}
 }
 
