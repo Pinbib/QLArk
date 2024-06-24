c3982ed9938332e0952b91b6393b134588938078
Pinbib <pancenkoe353@gmail.com>
2024-06-24 17:59:29 +0100

    added cs to Builders

--- src/Builders.js
+++ src/Builders.js
@@ -3,5 +3,6 @@ const cjs = require("./cjs");
 const mjs = require("./mjs");
 const ts = require("./ts");
 const py = require("./py");
+const cs = require("./cs");
 
-module.exports = {js, cjs, mjs, ts, py};
+module.exports = {js, cjs, mjs, ts, py, cs};
